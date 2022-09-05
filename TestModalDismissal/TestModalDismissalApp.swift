75f237b9611969108d73d4402089dceaf2fbbc91
Art Huang <art.huang@ui.com>
2022-09-05 15:00:07 +0800

    Initial Commit

---
+++ TestModalDismissal/TestModalDismissalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestModalDismissalApp.swift
+//  TestModalDismissal
+//
+//  Created by Art Huang on 2022/9/5.
+//
+
+import SwiftUI
+
+@main
+struct TestModalDismissalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
